1c328a2e23703a80266766e75f88b2894a5daa5c
vmural9 <vmural9@uic.edu>
2023-05-16 06:26:11 -0500

    added shadows and cycleText()

--- js/reviews.js
+++ js/reviews.js
@@ -6,7 +6,19 @@ function cycleLogo() {
 }
 logo1.addEventListener('transitionend', () => {
   logo1.classList.remove('translate');
-});
-logo2.addEventListener('transitionend', () => {
   logo2.classList.remove('translate');
+});
+
+function cycleText() {
+  let text1=document.getElementById("text1");
+  let text2=document.getElementById("text2")
+  text1.classList.add("translate");
+  text2.classList.add("translate");
+}
+text1.addEventListener('transitionend', () => {
+  temp = document.getElementById("text1").textContent;
+  document.getElementById("text1").textContent = document.getElementById("text2").textContent;
+  document.getElementById("text2").textContent = temp;
+  text1.classList.remove('translate');
+  text2.classList.remove('translate');
 });
